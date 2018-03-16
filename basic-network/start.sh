c515c5e9e8ea919502841f7d9700c113fa72f17a
phuongdv <dovietphuong.ptit@gmail.com>
2018-03-16 14:10:41 +0700

    update config

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -32,7 +32,7 @@ docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer channel 
 
 #docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051"  cli peer chaincode install -n mycc2 -v 1.0 -p github.com/chaincode_example02
 docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" cli peer chaincode install -n mycc -v 1.0 -p github.com/chaincode_example02
-docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051"  cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')"
+docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051"  cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051"  cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -v 1.0 -c '{"Args":["invoke","a","b","20"]}'
 docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" cli peer chaincode query -C mychannel -n mycc -c '{"Args":["query","a"]}'
 #
