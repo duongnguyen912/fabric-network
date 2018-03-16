13396beb9065921ace67edf04643b3154c055479
phuongdv <dovietphuong.ptit@gmail.com>
2018-03-16 12:01:22 +0700

    update config

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -30,7 +30,7 @@ docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" cli peer channel 
 docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer channel join -b mychannel.block
 
 
-
+#docker exec -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051"  cli peer chaincode install -n mycc -v 1.0 -p github.com/chaincode_example02
 docker exec cli peer chaincode install -n mycc -v 1.0 -p github.com/chaincode_example02
 docker exec cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')"
 docker exec cli peer chaincode query -C mychannel -n mycc -c '{"Args":["query","a"]}'
